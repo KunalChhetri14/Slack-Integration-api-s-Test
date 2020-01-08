3f36f10ddc096bb2ac274afed4cf7ff90560d19b
kunalchhetri14 <kunalchhetri14@gmail.com>
2020-01-08 12:48:01 +0530

    Cmd received and response from slack working properly

--- API's/app.js
+++ API's/app.js
@@ -1,6 +1,27 @@
 var express=require('express');
 const app=express();
 
+app.post('/addtodo',(req,res)=>{
+    console.log("Inside /addtodo api");
+    res.send("This is cool")
+})
+
+
+app.post('/marktodo',(req,res)=>{
+    console.log("inside /marktodo api");
+    res.send("todoDeleted");
+})
+
+app.post('/listtodos',(req,res)=>{
+    console.log("inside /listtodos api");
+    res.send("all todoList");
+})
+
+app.get('/',(req,res)=>{
+    res.send("hellow defula");
+})
+
+
 app.listen(3000,()=>{
     console.log("Server Conected");
 })
