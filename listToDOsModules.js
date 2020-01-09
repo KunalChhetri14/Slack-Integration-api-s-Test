2e054e8aac9f27f2557e5d3027ef5c80525fd9c4
kunalchhetri14 <kunalchhetri14@gmail.com>
2020-01-09 10:26:23 +0530

    Ran prettier script on listToDoModules

--- listToDOsModules.js
+++ listToDOsModules.js
@@ -1,53 +1,46 @@
-const url ='mongodb+srv://Kunal:mongodb123@clustertutorialspoint-o34i7.mongodb.net/test?retryWrites=true&w=majority';
-var MongoClient=require('mongodb').MongoClient;
-const dbName='ToDoAppDatbase';
-const toDoTableName='ToDoList';
+const url =
+  'mongodb+srv://Kunal:mongodb123@clustertutorialspoint-o34i7.mongodb.net/test?retryWrites=true&w=majority';
+var MongoClient = require('mongodb').MongoClient;
+const dbName = 'ToDoAppDatbase';
+const toDoTableName = 'ToDoList';
 
-module.exports.listToDoResponse=function(channelId){
-    return new Promise(function(resolve,reject){
+module.exports.listToDoResponse = function(channelId) {
+  return new Promise(function(resolve, reject) {
+    MongoClient.connect(url, function(err, db) {
+      if (err) {
+        console.log('Database connection error');
+        reject('Database side error');
+      } else {
+        console.log('connected');
+        var dbo = db.db(dbName);
+        let k = dbo
+          .collection(toDoTableName)
+          .find({ channel_id: channelId })
+          .toArray();
+        // console.log('K value is', k);
 
-        MongoClient.connect(url,function(err,db){
-            if(err){
-                console.log('Database connection error');
-                reject("Database side error");
-    
+        db.close();
+        k.then(data => {
+          ToDoItems = [];
+          // console.log("Dataq is ",data);
+          for (let value of data) {
+            ToDoItems.push(value.toDoItem);
+            //value.toDoItem);
+          }
+          let len = ToDoItems.length;
+          if (len == 0) {
+            resolve('NO TODOs');
+          } else {
+            let returnList = '';
+            for (let i of ToDoItems) {
+              returnList += '"' + i + '"   ';
             }
-    
-            else{
-                console.log("connected");
-                var dbo=db.db(dbName);
-                let k = dbo.collection(toDoTableName)
-                        .find({'channel_id':channelId})
-                        .toArray();
-                // console.log('K value is', k);
-    
-                db.close();
-                k.then(data => {
-                    ToDoItems=[];
-                   // console.log("Dataq is ",data);
-                    for(let value of data){
-                        ToDoItems.push(value.toDoItem);
-                        //value.toDoItem);
-                    }
-                    let len=ToDoItems.length;
-                    if(len==0){
-                        resolve("NO TODOs");
-                    }
-                    else{
-                        let returnList="";
-                        for(let i of ToDoItems){
-                            returnList+='"'+i+'"   ';
-                        }
-                        resolve(returnList);
-                    }
-                    
-                    })
-                .catch(err => {
-                       reject("Error in Server side ");
-                 });
-                
-                }
+            resolve(returnList);
+          }
+        }).catch(err => {
+          reject('Error in Server side ');
+        });
+      }
     });
-
-    })
-}
+  });
+};
